f65543363899add2d74d7dfb08f1c11a9ffba6b5
Franco Chong <fnbeddie@hotmail.com>
2017-02-04 14:42:04 -0500

    added content pages

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,27 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) =>{
-  let currentDate = new Date();
-  currentDate = currentDate.toLocaleTimeString();
-  res.render('index', { title: 'Express' , date: currentDate});
+  res.render('index', { title: 'Home' });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) =>{
-  res.render('index', { title: 'About' });
+  res.render('content/about', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', (req, res, next) =>{
+  res.render('content/projects', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) =>{
+  res.render('content/services', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) =>{
+  res.render('content/contact', { title: 'Contact' });
 });
 
 module.exports = router;
